bd17d3b848b31af7b0d566632f0656fcfda017dc
IcosagonZ <rohanraj3870z@gmail.com>
2025-10-04 12:13:52 +0530

    Added navigation using react router

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
-
+import { useNavigate } from 'react-router'
 
 function Home() {
-  return (
-    <>
-      <h1>Home</h1>
-    </>
-  )
+    const navigate = useNavigate();
+
+    return (
+        <>
+        <h1>Home</h1>
+        <div>
+            <button onClick={() => navigate("/")}>Home</button>
+            <button onClick={() => navigate("/results")}>Results</button>
+            <button onClick={() => navigate("/paper")}>Paper</button>
+        </div>
+        </>
+    )
 }
 
 export default Home
--- src/pages/Paper.jsx
+++ src/pages/Paper.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
-
+import { useNavigate } from 'react-router'
 
 function Paper() {
-  return (
-    <>
-      <h1>Paper</h1>
-    </>
-  )
+    const navigate = useNavigate();
+
+    return (
+        <>
+        <h1>Paper</h1>
+        <div>
+            <button onClick={() => navigate("/")}>Home</button>
+            <button onClick={() => navigate("/results")}>Results</button>
+            <button onClick={() => navigate("/paper")}>Paper</button>
+        </div>
+        </>
+    )
 }
 
 export default Paper
